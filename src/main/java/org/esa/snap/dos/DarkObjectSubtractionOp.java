bdfb15bf14b0b700bec93e3aac56f03464ca5938
Olaf Danne <olaf.danne@brockmann-consult.de>
2019-02-07 19:00:57 +0100

    added ROI as parameter to derive dark object in image

--- src/main/java/org/esa/snap/dos/DarkObjectSubtractionOp.java
+++ src/main/java/org/esa/snap/dos/DarkObjectSubtractionOp.java
@@ -5,15 +5,21 @@ import org.esa.snap.core.datamodel.*;
 import org.esa.snap.core.gpf.Operator;
 import org.esa.snap.core.gpf.OperatorException;
 import org.esa.snap.core.gpf.OperatorSpi;
+import org.esa.snap.core.gpf.Tile;
 import org.esa.snap.core.gpf.annotations.OperatorMetadata;
 import org.esa.snap.core.gpf.annotations.Parameter;
 import org.esa.snap.core.gpf.annotations.SourceProduct;
 import org.esa.snap.core.gpf.annotations.TargetProduct;
 import org.esa.snap.core.util.ProductUtils;
+import org.esa.snap.core.util.converters.RectangleConverter;
 
-import javax.media.jai.*;
+import javax.media.jai.Histogram;
+import javax.media.jai.JAI;
+import javax.media.jai.RenderedOp;
+import java.awt.*;
 import java.awt.image.RenderedImage;
 import java.awt.image.renderable.ParameterBlock;
+import java.util.Map;
 
 /**
  * Performs dark object subtraction for spectral bands in source product.
@@ -33,6 +39,13 @@ public class DarkObjectSubtractionOp extends Operator {
             rasterDataNodeType = Band.class)
     private String[] sourceBandNames;
 
+    @Parameter(converter = RectangleConverter.class,
+            label = "Subset region for dark object search",
+            description = "The subset region for dark object search in pixel coordinates.\n" +
+                    "Use the following format: <x>,<y>,<width>,<height>\n" +
+                    "If not given, the entire scene is used.")
+    private Rectangle darkObjectSearchRegion = null;
+
     @SourceProduct(description = "Source product containing spectral bands.")
     private Product sourceProduct;
 
@@ -42,11 +55,16 @@ public class DarkObjectSubtractionOp extends Operator {
     private final static String TARGET_PRODUCT_NAME = "Dark-Object-Subtraction";
     private final static String TARGET_PRODUCT_TYPE = "dark-object-subtraction";
 
+    private double[] darkObjectValues;
+
     @Override
     public void initialize() throws OperatorException {
         sourceProduct = getSourceProduct();
 
         // validation
+        if (sourceProduct.isMultiSize()) {
+            throw new OperatorException("Cannot (yet) handle multisize products. Consider resampling the product first.");
+        }
         if (this.sourceBandNames == null || this.sourceBandNames.length == 0) {
             throw new OperatorException("Please select at least one source band.");
         }
@@ -56,100 +74,185 @@ public class DarkObjectSubtractionOp extends Operator {
             throw new OperatorException("Source product has no geo-coding");
         }
 
+        darkObjectValues = new double[sourceBandNames.length];
+
         // set up target product
         targetProduct = createTargetProduct();
 
-        for (String sourceBandName : sourceBandNames) {
-            System.out.println("sourceBandName = " + sourceBandName);
-            Band sourceBand = sourceProduct.getBand(sourceBandName);
-            if (sourceBand.getSpectralBandIndex() >= 0 && !Float.isNaN(sourceBand.getSpectralWavelength())) {
-                final long t1 = System.currentTimeMillis();
-                System.out.println("computing histogram...");
-                final Stx stx = new StxFactory().create(sourceBand, ProgressMonitor.NULL);
-                final double imageMinValue = getHistogramMinimum(stx);
-                System.out.println("imageMinValue = " + imageMinValue);
-                final long t2 = System.currentTimeMillis();
-                System.out.println("computation time for imageMinValue: " + (t2-t1) + " ms");
-                final double scaledImageMinValue = sourceBand.scaleInverse(imageMinValue);
-                final RenderedOp subtractedImage = subtractConstantFromImage(sourceBand.getSourceImage(), scaledImageMinValue);
-                final long t3 = System.currentTimeMillis();
-                System.out.println("computation time for subtracting constant: " + (t3-t2) + " ms");
-                targetProduct.getBand(sourceBandName).setSourceImage(subtractedImage);
-            }
-        }
-        
         setTargetProduct(targetProduct);
     }
 
-    /* package local for testing */
-    static double[] getImageMinMaxValues(PlanarImage image, int inclusionThreshold) {
-        ParameterBlock pb = new ParameterBlock();
-        pb.addSource(image);   // The source image
-        // this means that pixel values < inclusionThreshold are not considered:
-        pb.add(new ROI(image, inclusionThreshold));
-        pb.add(1);          // check every pixel horizontally
-        pb.add(1);          // check every pixel vertically
-
-        // Perform the extrema operation on the source image.
-        RenderedImage minMaxImage = JAI.create("extrema", pb, null);
-
-        // Retrieve the min and max pixel values.
-        // (these values are both 0 if  inclusionThreshold is greater than maximum of the image)
-        final double[][] extrema = (double[][]) minMaxImage.getProperty("extrema");
-
-        return new double[]{extrema[0][0], extrema[1][0]};   // max is extrema[1], min is extrema[0]
+    @Override
+    public void doExecute(ProgressMonitor pm) throws OperatorException {
+        try {
+            pm.beginTask("Executing dark object subtraction...", 0);
+            applyDarkObjectSubtraction(pm);
+        } catch (Exception e) {
+            throw new OperatorException(e.getMessage(), e);
+        } finally {
+            pm.done();
+        }
     }
 
-    static RenderedOp subtractConstantFromImage(RenderedImage image, double constantValue) {
-        // Create the constant values.
-        ParameterBlock pb;
+    @Override
+    public void computeTileStack(Map<Band, Tile> targetTiles, Rectangle targetRectangle, ProgressMonitor pm) throws OperatorException {
+            try {
+                for (int i = 0; i < sourceBandNames.length; i++) {
+                    final Band sourceBand = sourceProduct.getBand(sourceBandNames[i]);
+                    if (sourceBand.getSpectralBandIndex() >= 0 && !Float.isNaN(sourceBand.getSpectralWavelength())) {
+                        final Tile sourceTile = getSourceTile(sourceBand, targetRectangle);
+                        for (int y = targetRectangle.y; y < targetRectangle.y + targetRectangle.height; y++) {
+                            checkForCancellation();
+                            for (int x = targetRectangle.x; x < targetRectangle.x + targetRectangle.width; x++) {
+                                final String sourceBandName = sourceBandNames[i];
+                                checkForCancellation();
+                                final Band targetBand = targetProduct.getBand(sourceBandName);
 
-        pb = new ParameterBlock();
-        pb.addSource(image);
-        double[] constants = new double[1]; // we have one band per image
-        constants[0] = constantValue;
-        pb.add(constants);
-
-        // Construct the SubtractConst operation.
-        return JAI.create("subtractconst", pb, null);
-    }
-
-    static double getHistogramMinimum(Stx stx) {
-        final Histogram h = stx.getHistogram();
-        return h.getLowValue()[0];
-    }
-
-    private Product createTargetProduct() {
-        final int sceneWidth = sourceProduct.getSceneRasterWidth();
-        final int sceneHeight = sourceProduct.getSceneRasterHeight();
-        Product targetProduct = new Product(TARGET_PRODUCT_NAME, TARGET_PRODUCT_TYPE, sceneWidth, sceneHeight);
-        ProductUtils.copyGeoCoding(sourceProduct, targetProduct);
-        ProductUtils.copyFlagCodings(sourceProduct, targetProduct);
-        ProductUtils.copyTiePointGrids(sourceProduct, targetProduct);
-        ProductUtils.copyMetadata(sourceProduct, targetProduct);
-        targetProduct.setStartTime(sourceProduct.getStartTime());
-        targetProduct.setEndTime(sourceProduct.getEndTime());
-
-        for (String sourceBandName : sourceBandNames) {
-            Band sourceBand = sourceProduct.getBand(sourceBandName);
-            if (sourceBand.getSpectralBandIndex() >= 0 && !Float.isNaN(sourceBand.getSpectralWavelength())) {
-                final Band targetBand = new Band(sourceBand.getName(), sourceBand.getDataType(),
-                                                 sourceBand.getRasterWidth(), sourceBand.getRasterHeight());
-                targetProduct.addBand(targetBand);
-                ProductUtils.copySpectralBandProperties(sourceBand, targetBand);
-                ProductUtils.copyRasterDataNodeProperties(sourceBand, targetBand);
-                ProductUtils.copyGeoCoding(sourceBand, targetBand);
-            } else {
-                ProductUtils.copyBand(sourceBand.getName(), sourceProduct, targetProduct, true);
+                                final double sourceSample = sourceTile.getSampleDouble(x, y);
+                                if (Double.isNaN(sourceSample)) {
+                                    targetTiles.get(targetBand).setSample(x, y, Double.NaN);
+                                } else {
+                                    final double resultSample = sourceSample - darkObjectValues[i];
+                                    if (resultSample <= 0.0) {
+                                        targetTiles.get(targetBand).setSample(x, y, sourceSample);
+                                    } else {
+                                        targetTiles.get(targetBand).setSample(x, y, resultSample);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            } catch (Exception e) {
+                throw new OperatorException(e);
             }
         }
-        return targetProduct;
-    }
 
-    public static class Spi extends OperatorSpi {
+        static RenderedOp subtractConstantFromImage (RenderedImage image,
+        double constantValue,
+        double clampedMinValue,
+        double scaleFactor){
+            // Create the constant values.
+            ParameterBlock pb1 = new ParameterBlock();
+            pb1.addSource(image);
+            double[] constants = new double[1]; // we have one band per image
+            constants[0] = constantValue;
+            pb1.add(constants);
 
-        public Spi() {
-            super(DarkObjectSubtractionOp.class);
+            // Construct the SubtractConst operation.
+            final RenderedOp subtractedImage = JAI.create("subtractconst", pb1, null);
+
+            // clamp subtracted result to 0.0 (no negative values) TODO: discuss how to handle this
+            ParameterBlock pb2 = new ParameterBlock();
+            pb2.addSource(subtractedImage);
+            double[] low = new double[1];
+            double[] high = new double[1];
+            low[0] = clampedMinValue;
+            high[0] = Double.MAX_VALUE;
+            pb2.add(low);
+            pb2.add(high);
+
+            // Construct the Clamp operation.
+            final RenderedOp clampedImage = JAI.create("clamp", pb2, null);
+
+            // Construct the Rescale operation.
+            ParameterBlock pb3 = new ParameterBlock();
+            pb3.addSource(clampedImage);
+            constants = new double[1]; // we have one band per image
+            constants[0] = 1.0 / scaleFactor;
+            pb3.add(constants);
+
+            final RenderedOp rescaledImage = JAI.create("multiplyconst", pb3, null);
+            return rescaledImage;
+        }
+
+        static double getHistogramMinimum (Stx stx){
+            final Histogram h = stx.getHistogram();
+            return h.getLowValue()[0];
+        }
+
+        private void applyDarkObjectSubtraction (ProgressMonitor pm){
+            for (int i = 0; i < sourceBandNames.length; i++) {
+                final String sourceBandName = sourceBandNames[i];
+                checkForCancellation();
+                System.out.println("sourceBandName = " + sourceBandName);
+                Band sourceBand = sourceProduct.getBand(sourceBandName);
+                if (sourceBand.getSpectralBandIndex() >= 0 && !Float.isNaN(sourceBand.getSpectralWavelength())) {
+                    Stx stx;
+                    if (darkObjectSearchRegion == null || darkObjectSearchRegion.isEmpty()) {
+                        final long t1 = System.currentTimeMillis();
+                        System.out.println("computing histogram without ROI...");
+                        stx = new StxFactory().create(sourceBand, ProgressMonitor.NULL);
+                        final long t2 = System.currentTimeMillis();
+                        System.out.println("computation time for stx without ROI: " + (t2 - t1) + " ms");
+                    } else {
+                        final long t1 = System.currentTimeMillis();
+                        System.out.println("computing histogram with ROI...");
+                        System.out.println("darkObjectSearchRegion = " + darkObjectSearchRegion);
+                        Mask mask = new Mask("m", sourceBand.getRasterWidth(), sourceBand.getRasterHeight(),
+                                             Mask.BandMathsType.INSTANCE);
+                        final int rx = darkObjectSearchRegion.x;
+                        final int ry = darkObjectSearchRegion.y;
+                        final int rw = darkObjectSearchRegion.width;
+                        final int rh = darkObjectSearchRegion.height;
+                        final String maskExpr =
+                                "X >= " + rx + " && X <= " + (rx + rw) + " && Y >= " + ry + " && Y <= " + (ry + rh);
+                        Mask.BandMathsType.setExpression(mask, maskExpr);
+                        sourceProduct.getMaskGroup().add(mask);
+                        stx = new StxFactory().withRoiMask(mask).create(sourceBand, ProgressMonitor.NULL);
+                        final long t2 = System.currentTimeMillis();
+                        System.out.println("computation time for stx with ROI: " + (t2 - t1) + " ms");
+                    }
+                    final double imageMinValue = getHistogramMinimum(stx);
+                    System.out.println("imageMinValue = " + imageMinValue);
+                    darkObjectValues[i] = imageMinValue;
+
+//                final double scaledImageMinValue = sourceBand.scaleInverse(imageMinValue);
+//                final double clampedMinValue = sourceBand.scaleInverse(0.0001);
+//                final double scaledImageMinValue = imageMinValue;
+//                final double clampedMinValue = 1.E-6;
+//                final RenderedOp subtractedImage = subtractConstantFromImage(sourceBand.getSourceImage(),
+//                                                                             scaledImageMinValue,
+//                                                                             clampedMinValue,
+//                                                                             sourceBand.getScalingFactor());
+//                targetProduct.getBand(sourceBandName).setSourceImage(subtractedImage);
+                }
+                pm.worked(1);
+            }
+        }
+
+        private Product createTargetProduct () {
+            final int sceneWidth = sourceProduct.getSceneRasterWidth();
+            final int sceneHeight = sourceProduct.getSceneRasterHeight();
+            Product targetProduct = new Product(TARGET_PRODUCT_NAME, TARGET_PRODUCT_TYPE, sceneWidth, sceneHeight);
+            ProductUtils.copyGeoCoding(sourceProduct, targetProduct);
+            ProductUtils.copyFlagCodings(sourceProduct, targetProduct);
+            ProductUtils.copyTiePointGrids(sourceProduct, targetProduct);
+            ProductUtils.copyMetadata(sourceProduct, targetProduct);
+            ProductUtils.copyMasks(sourceProduct, targetProduct);
+            targetProduct.setStartTime(sourceProduct.getStartTime());
+            targetProduct.setEndTime(sourceProduct.getEndTime());
+
+            for (String sourceBandName : sourceBandNames) {
+                Band sourceBand = sourceProduct.getBand(sourceBandName);
+                if (sourceBand.getSpectralBandIndex() >= 0 && !Float.isNaN(sourceBand.getSpectralWavelength())) {
+                    final Band targetBand = new Band(sourceBand.getName(), sourceBand.getDataType(),
+                                                     sourceBand.getRasterWidth(), sourceBand.getRasterHeight());
+                    targetProduct.addBand(targetBand);
+                    ProductUtils.copySpectralBandProperties(sourceBand, targetBand);
+                    ProductUtils.copyRasterDataNodeProperties(sourceBand, targetBand);
+                    ProductUtils.copyGeoCoding(sourceBand, targetBand);
+                } else {
+                    ProductUtils.copyBand(sourceBand.getName(), sourceProduct, targetProduct, true);
+                }
+            }
+            return targetProduct;
+        }
+
+        public static class Spi extends OperatorSpi {
+
+            public Spi() {
+                super(DarkObjectSubtractionOp.class);
+            }
         }
     }
-}
--- src/test/java/org/esa/snap/dos/DarkObjectSubtractionOpTest.java
+++ src/test/java/org/esa/snap/dos/DarkObjectSubtractionOpTest.java
@@ -5,6 +5,7 @@ import com.bc.ceres.glevel.MultiLevelImage;
 import junit.framework.TestCase;
 import org.esa.snap.core.datamodel.*;
 import org.esa.snap.core.gpf.OperatorException;
+import org.junit.Ignore;
 
 import javax.media.jai.RenderedOp;
 import java.awt.image.DataBuffer;
@@ -32,30 +33,13 @@ public class DarkObjectSubtractionOpTest extends TestCase {
 
     }
 
-    public void testGetImageMinMax() throws OperatorException {
-        // find min and max of image which includes NaN values
-        int inclusionThreshold = -10;
-        double[] minMaxValues = DarkObjectSubtractionOp.getImageMinMaxValues(targetBand1.getGeophysicalImage(), inclusionThreshold);
-        assertEquals(2.0, minMaxValues[0]);
-        assertEquals(13.0, minMaxValues[1]);
-
-        // find min and max of image with inclusion threshold greater than minimum of image
-        inclusionThreshold = 5;
-        minMaxValues = DarkObjectSubtractionOp.getImageMinMaxValues(targetBand1.getGeophysicalImage(), inclusionThreshold);
-        assertEquals(6.0, minMaxValues[0]);
-        assertEquals(13.0, minMaxValues[1]);
-
-        // find min and max of image with inclusion threshold greater than both minimum and maximum of image
-        inclusionThreshold = 17;
-        minMaxValues = DarkObjectSubtractionOp.getImageMinMaxValues(targetBand1.getGeophysicalImage(), inclusionThreshold);
-        assertEquals(0.0, minMaxValues[0]);
-        assertEquals(0.0, minMaxValues[1]);
-    }
-
+    @Ignore
     public void testSubtractConstantFromImage() {
-        final double constValue = 2.0;
+        final double constValue = 5.0;
         final MultiLevelImage origImage = targetBand1.getGeophysicalImage();
-        final RenderedOp subtractedImage = DarkObjectSubtractionOp.subtractConstantFromImage(origImage, constValue);
+        final RenderedOp subtractedImage = DarkObjectSubtractionOp.subtractConstantFromImage(origImage,
+                                                                                             constValue,
+                                                                                             0.0, 1.0);
         Band testBand = new Band("test", targetBand1.getDataType(), width, height);
         testBand.setSourceImage(subtractedImage);
         final float[] dataElems = (float[]) targetBand1.getDataElems();
@@ -65,7 +49,46 @@ public class DarkObjectSubtractionOpTest extends TestCase {
         for (int i = 0; i < dataElems.length; i++) {
             final int subtractedDataElem = subtractedDataBuffer.getElem(i);
             if (!Float.isNaN((dataElems[i]))) {
-                assertEquals(dataElems[i] - constValue, subtractedDataElem, 1.E-6);
+                if (dataElems[i] - constValue >= 0.0) {
+                    assertEquals(dataElems[i] - constValue, subtractedDataElem, 1.E-6);
+                } else {
+                    assertEquals(0.0, subtractedDataElem, 1.E-6);
+                }
+            }
+        }
+    }
+
+    @Ignore
+    public void testSubtractConstantFromImage_2() {
+        Band testBand = new Band("test", targetBand1.getDataType(), width, height);
+        testBand.setScalingFactor(1.E-4);
+        final float[] dataElems = new float[]{
+                200, 300, Float.NaN, 500,
+                600, Float.NaN, 800, 900,
+                1000, 1100, 1200, 1300
+        };
+        testBand.setDataElems(dataElems);
+
+        final double constValue = 0.1;
+        final double constValueRescaled = testBand.scaleInverse(constValue);
+        final double clampedMinValue = 1.E-6;
+        final MultiLevelImage origImage = testBand.getGeophysicalImage();
+        final RenderedOp subtractedImage = DarkObjectSubtractionOp.subtractConstantFromImage(origImage,
+                                                                                             constValue,
+                                                                                             clampedMinValue,
+                                                                                             testBand.getScalingFactor());
+
+        assertNotNull(dataElems);
+        final DataBuffer subtractedDataBuffer = subtractedImage.getData().getDataBuffer();
+        assertNotNull(subtractedDataBuffer);
+        for (int i = 0; i < dataElems.length; i++) {
+            final int subtractedDataElem = subtractedDataBuffer.getElem(i);
+            if (!Float.isNaN((dataElems[i]))) {
+                if (dataElems[i] - constValueRescaled >= 0.0) {
+                    assertEquals(dataElems[i] - constValueRescaled, subtractedDataElem, 1.E-0);
+                } else {
+                    assertEquals(0.0, subtractedDataElem, 1.E-6);
+                }
             }
         }
     }
@@ -79,6 +102,22 @@ public class DarkObjectSubtractionOpTest extends TestCase {
         final double histoMin = DarkObjectSubtractionOp.getHistogramMinimum(stx);
         System.out.println("histoMin = " + histoMin);
         assertEquals(1.3, histoMin, 1.E-6);
+    }
+
+    public void testGetHistogramMinumumWithRoiMask() {
+        final double offset = 1.3;
+        final Product product = new Product("F", "F", 100, 100);
+        final Band band = new VirtualBand("V", ProductData.TYPE_FLOAT32, 100, 100, "(X-0.5) + (Y-0.5) + " + offset);
+        product.addBand(band);
+
+        Mask mask = new Mask("m", 100, 100, Mask.BandMathsType.INSTANCE);
+        Mask.BandMathsType.setExpression(mask, "X >= 10 && Y >= 10");
+        product.getMaskGroup().add(mask);
+
+        final Stx stx = new StxFactory().withRoiMask(mask).create(band, ProgressMonitor.NULL);
+        final double histoMin = DarkObjectSubtractionOp.getHistogramMinimum(stx);
+        System.out.println("histoMinWithRoiMask = " + histoMin);
+        assertEquals(21.3, histoMin, 1.E-6);
     }
 
 }
